f2a5bcf15950819f6328375c7f1ecf5188933dc8
coledavisdevelops <colebuildanddevelop@gmail.com>
2023-05-01 16:29:28 -0500

    runs at least

--- environment.py
+++ environment.py
@@ -2,18 +2,19 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 
-class TradingEnvironment(gym.Env):
+
+class SimpleTradingEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, data):
-        super(TradingEnvironment, self).__init__()
+        super(SimpleTradingEnv, self).__init__()
         self.data = data
         self.current_step = 0
         self.profit = 0
 
         # Define action and observation space
-        self.action_space = spaces.Discrete(3) # 0: Buy, 1: Sell, 2: Hold
-        self.observation_space = spaces.Box(low=0, high=np.inf, shape=(4,))
+        self.action_space = gym.spaces.Discrete(3)  # 0: Buy, 1: Sell, 2: Hold
+        self.observation_space = gym.spaces.Box(low=0, high=np.inf, shape=(3,))
 
     def reset(self):
         self.current_step = 0
@@ -22,20 +23,20 @@ class TradingEnvironment(gym.Env):
 
     def _next_observation(self):
         obs = self.data[self.current_step]
-        return obs
+        return np.array([obs])
 
     def step(self, action):
         reward = 0
-        if action == 0: # Buy
+        if action == 0:  # Buy
             self.profit -= self.data[self.current_step]
-        elif action == 1: # Sell
+        elif action == 1:  # Sell
             self.profit += self.data[self.current_step]
-        else: # Hold
+        else:  # Hold
             pass
 
         self.current_step += 1
 
-        done = self.current_step == len(self.data)
+        done = self.current_step == len(self.data) - 1
         if done:
             reward = self.profit
 
--- main.py
+++ main.py
@@ -1,11 +1,50 @@
-from environment import TradingEnvironment
-from data import data
+from environment import SimpleTradingEnv
+import numpy as np
+import random
 
-env = TradingEnvironment(data)
 
-obs = env.reset()
+def data(length): return [random.randint(0, 10000) for _ in range(length)]
+
+
+env = SimpleTradingEnv(data(2000))
+
+# Initialize the Q-table
+q_table = np.zeros((env.observation_space.shape[0], env.action_space.n))
+# [[0, 0, 0]]
+
+
+# Set the hyperparameters
+alpha = 0.1  # learning rate
+gamma = 0.9  # discount factor
+epsilon = 0.1  # exploration rate
+
+# Run the Q-learning algorithm
+for episode in range(1000):
+    env.reset()
+    done = False
+
+    while not done:
+        # Choose an action using an epsilon-greedy policy
+        if np.random.uniform(0, 1) < epsilon:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(q_table[0])
+
+        # Take the chosen action and observe the next state and reward
+        next_state, reward, done, _ = env.step(action)
+
+        # Update the Q-value of the current state-action pair
+        q_table[0, action] = (1 - alpha) * q_table[0, action] + alpha * (
+            reward + gamma * np.max(q_table[0]))
+
+# Evaluate the performance of the agent
+env.reset()
 done = False
+total_reward = 0
+
 while not done:
-    action = env.action_space.sample()
-    obs, reward, done, info = env.step(action)
-    env.render()
+    action = np.argmax(q_table[0])
+    state, reward, done, _ = env.step(action)
+    total_reward += reward
+
+print("Total profit: {:.2f}".format(total_reward))
